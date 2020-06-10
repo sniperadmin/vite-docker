4e9e545733855dc19245db8a7c352ed95630e86e
Nasr Galal <nasrbue@gmail.com>
2020-06-10 20:46:01 +0200

    initial setup

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <button @click="count++">count is: {{ count }}</button>
+  <p>This is a simple app</p>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
